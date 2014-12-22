06f15f1f774fe304b1704a7f05e5ef7dc50d45a7
Daniel <dspinoz@gmail.com>
2014-12-23 09:14:28 +1030

    just put into logs

--- config/pmmgr.js
+++ config/pmmgr.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  archives: process.env.ARCHIVE_DIR || 'logs/pmmgr',
+  archives: process.env.ARCHIVE_DIR || 'logs',
   config: process.env.PMMGR_CONFIG_DIR || 'config/pmmgr'
 
 }
